ea1c1c2d76d605fea0daf4856726dabf86ff2e0f
Anderson Guimarães <anderson_sgg@hotmail.com>
2024-04-08 02:03:37 -0300

    Add project files.

---
+++ ReadImageWithTesseract/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using Tesseract;
+
+namespace ReadImageWithTesseract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("OCR Console App");
+
+            var filepath = "C:\\Projetos\\ReadImageWithTesseract\\";
+            var imagePath = @$"{ filepath }image.png";
+
+            using var engine = new TesseractEngine(@"C:\Projetos\ReadImageWithTesseract\ReadImageWithTesseract\tessdata\", "eng", EngineMode.Default);
+            using var img = Pix.LoadFromFile(imagePath);
+            using var page = engine.Process(img);
+
+            var text = page.GetText();
+                        
+            
+            Console.WriteLine("Texto encontrado na imagem:");
+            Console.WriteLine(text);
+                 
+
+        }
+    }
+}
